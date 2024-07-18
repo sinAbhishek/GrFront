4856ed8a03861aa8d07bcff1230726e638e793f0
sinAbhishek <sinabhshek979@gmail.com>
2024-07-18 18:27:14 +0530

    final

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext, useRef } from "react";
 import io from "socket.io-client";
 import { useRouter } from "next/navigation";
 import { AuthContext } from "@/components/state";
@@ -28,7 +28,7 @@ import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 import { StyledEngineProvider } from "@mui/material";
 import Lobby from "@/components/Home";
-import { animate, motion, useAnimate } from "framer-motion";
+import { animate, motion, useAnimate, useInView } from "framer-motion";
 // import {Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure,extendVariants,Input} from "@nextui-org/react";
 export default function Home() {
   const style = {
@@ -49,6 +49,7 @@ export default function Home() {
     slidesToShow: 3,
     slidesToScroll: 1,
   };
+  const ref = useRef(null);
   const [data, setdata] = useState([]);
   const [sliderscope, animateslider] = useAnimate();
   const [slideshow, setslideshow] = useState(false);
@@ -58,6 +59,7 @@ export default function Home() {
   const [openlobby, setopenlobby] = useState(false);
   const [room, setRoom] = useState("");
   const [password, setPassword] = useState("");
+  const isInView = useInView(ref, { amount: 0.2 });
   const [path, setpath] = useState("");
   const { Socket, dispatch, rooms, joinedroom, err, name } =
     useContext(AuthContext);
@@ -72,6 +74,36 @@ export default function Home() {
   const [crews, setcrews] = useState([]);
   const ENDPOINT = "http://localhost:5000";
 
+  const mainvariants = {
+    hidden: { x: "-100vw" },
+    visible: {
+      x: 0,
+      transition: {
+        delay: 0.5,
+        when: "beforeChildren",
+        staggerChildren: 0.5,
+        duration: 0.8,
+        type: "spring",
+      },
+    },
+  };
+  const childvariants = {
+    hidden: { x: 0, opacity: 0, rotate: 0 },
+    visible: {
+      x: 0,
+      opacity: 1,
+      rotate: 0,
+      transition: { type: "linear", duration: 0.4, stiffness: 400 },
+    },
+    axe: {
+      x: 0,
+      opacity: 1,
+      rotate: 0,
+    },
+  };
+  useEffect(() => {
+    console.log(isInView);
+  }, [isInView]);
   useEffect(() => {
     if (joinedroom !== null) {
       Socket &&
@@ -220,14 +252,15 @@ export default function Home() {
   };
   return (
     <>
-      <div className=" marcellus w-screen h-max relative">
-        <Lobby blogs={data} setslideshow={close} openmodal={openLobby} />
+      <div className="home marcellus w-screen h-screen relative flex justify-between">
+        {/* <Lobby blogs={data} setslideshow={close} openmodal={openLobby} /> */}
+
         <motion.div
           ref={sliderscope}
           variants={slidervariant}
           initial={"hidden"}
           animate={slideshow && "visible"}
-          className=" w-[30%] flex flex-col items-center bg-slate-400 h-screen lobby-main absolute left-0 top-0 py-4 z-30"
+          className=" w-[40%] min-w-[450px] max-[500px]:min-w-[100%] flex flex-col items-center bg-slate-400 h-screen lobby-main absolute left-0 top-0 py-4 z-30"
         >
           <button
             onClick={() => animateslider(sliderscope.current, { x: "-100%" })}
@@ -273,73 +306,61 @@ export default function Home() {
             </div>
           )}
         </motion.div>
-        {/* // <main className="marcellus flex h-screen justify-between  w-screen main"> */}
 
-        {/* <div className=" w-[30%] flex flex-col items-center bg-slate-400 h-full lobby-main ">
-      <div className=" top_bar justify-between">
-            <h4 className="text-[#e6cda1] font-medium">Lobbies</h4>
-            <button onClick={()=>setopenlobby(true)} className=" bg-red-300 px-2 py-1 rounded-full md:hidden ">Create lobby</button>
-          </div>
-          {rooms?rooms.map((c,i)=> <div key={i} className="  w-[95%]   h-12 my-2 rounded-full relative overflow-hidden">
-          <div
-            style={{
-              backgroundImage: `  linear-gradient(to right,rgba(245, 243, 237,1)10%,rgba(245, 243, 237,0)80%),url("./eulanamecard.png")`,
-            }}
-            className="absolute right-0 top-0 bottom-0 w-[50%] lobby_info "
-          ></div>
-          <div className="  setbg w-full  h-full flex justify-between items-center z-20 px-4  ">
-            <div className=" w-1/3">
-              <p>{Object.keys(c)[0]}</p>
+        <div className=" w-full relative flex justify-center items-center">
+          <div className=" min-w-[450px] max-[450px]:min-w-[98%] w-[40%] h-[400px] relative bg-white bg-opacity-15">
+            <img
+              src="./paimon2.png"
+              alt=""
+              className=" w-[150px] h-[150px] absolute -top-24 left-[150px] z-50 "
+            />
+            <div
+              onClick={() => close()}
+              className=" hover:cursor-pointer desc w-[200px] h-[50px] absolute -top-[50px] left-0  flex justify-center items-center "
+            >
+              <p className="  text-white font-medium z-20">JOIN LOBBY</p>
             </div>
-            <div className=" w-1/3 z-10">
-              <p>{Object.values(c)[0].length}/4</p>
-            </div>
-  
-       <button onClick={()=>PassModal(Object.keys(c)[0])} className="btn-primary   ">
-              Join
-            </button>
-          
-           
-          </div>
-        </div>):<div className=" w-full h-full flex flex-col justify-center items-center">
-          <div className=" w-[100px] h-[100px] ">
-         <img className=" bg-cover" src="./qiqialt.png" alt="" />
-          </div>
-      <p className=" text-white font-medium">No lobbies</p>
-          </div>}
- 
-      </div>
+            <div className=" w-full h-full flex flex-col items-center">
+              <div className=" relative lobbyDetails bg-opacity-10  backdrop-blur-lg  bg-white w-[100%] h-[80%]  rounded-md flex flex-col justify-center items-center">
+                <img
+                  src="./hoyoverse-logo.png"
+                  className=" w-[280px] h-[50px] mb-8 mt-4"
+                  alt=""
+                />
+                <input
+                  type="text"
+                  className=" my-2 h-11 w-[300px] outline-none border border-slate-300 rounded-md text-slate-900 px-2 "
+                  placeholder="Enter lobby name"
+                  onChange={(event) => setRoom(event.target.value)}
+                />
+                <input
+                  type="password"
+                  className=" h-11 w-[300px] outline-none border border-slate-300 rounded-md text-slate-900 px-2 "
+                  placeholder="Enter lobby password"
+                  onChange={(event) => setPassword(event.target.value)}
+                />
+                <button
+                  disabled={loading}
+                  onClick={() => call(room)}
+                  className=" btn-lobby w-[300px]"
+                >
+                  Create lobby
+                </button>
 
-      <div className="w-[75%] h-full main-right ">
-        <div className=" flex flex-col justify-center items-center w-full h-full">
-        <div className="create-lobby ">
-          <div className=" top_bar">
-            <h4 className="text-[#e6cda1] font-medium">Create Lobby</h4>
+                {loading && (
+                  <div className=" w-full flex justify-center items-center relative">
+                    <div class="lds-ring">
+                      <div></div>
+                      <div></div>
+                      <div></div>
+                      <div></div>
+                    </div>
+                  </div>
+                )}
+              </div>
+            </div>
           </div>
-          <div className=" relative lobbyDetails bg-opacity-10  backdrop-blur-lg  bg-white w-[50%] h-[80%] my-2 rounded-md flex flex-col justify-center items-center">
-            <img src="./hoyoverse-logo.png" className=" w-[250px] h-[50px] mb-8 mt-4" alt="" />
-            <input type="text" className=" input-lobby" placeholder="Enter lobby name"   onChange={(event) => setRoom(event.target.value)} />
-            <input type="password" className=" input-lobby" placeholder="Enter lobby password"   onChange={(event) => setPassword(event.target.value)} />
-            <button disabled={loading} onClick={()=>call(room)} className=" btn-lobby">Create lobby</button>
-            {loading&&<div class="lds-ring"><div></div><div></div><div></div><div></div></div>}
-          </div>
-</div>
-<div className=" blog">
-<div className=" top_bar">
-            <h4 className="text-[#e6cda1] font-medium">Blogs</h4>
-         
-          </div>
-          <div className="sliderCont w-full h-[50%] mt-2 ">
-          <Slider {...settings}>
-          {data&&data.map((c,i)=>  <Blog key={i} blogs={c}/>)}
-            </Slider>
-        
-          </div>
-       
-</div>
         </div>
-       
-      </div> */}
         <StyledEngineProvider injectFirst>
           <Modal
             open={isOpen}
@@ -498,6 +519,29 @@ export default function Home() {
           </Modal>
         </StyledEngineProvider>
       </div>
+      <section className="sec2 w-screen h-screen flex flex-col justify-center items-center  p-0 m-0 backdrop-blur-md">
+        <div
+          ref={ref}
+          className=" w-full h-full flex justify-center items-center"
+        >
+          <motion.div
+            variants={mainvariants}
+            initial="hidden"
+            animate={isInView && "visible"}
+            className=" overflow-y-scroll blogcont  relative flex  items-center justify-center w-[90%] h-[90%] md:w-[80%] md:h-[80%]  bg-opacity-10 p-2 backdrop-blur-md  "
+          >
+            <h2 className=" marcellus text-3xl font-bold text-orange-700 absolute top-0 left-0 w-max  px-2 py-1 rounded-sm ">
+              Genshin News
+            </h2>
+            <div className="w-full h-[80%] flex justify-center flex-wrap">
+              {data[0] &&
+                data.map((c, i) => (
+                  <Blog variantname={childvariants} key={i} blogs={c} />
+                ))}
+            </div>
+          </motion.div>
+        </div>
+      </section>
     </>
   );
   // w-max z-10 bg-orange-100 px-4 py-1 rounded-full
