ed9d48a8c587331992f0c4b31ac56b0bb88ad518
sinAbhishek <sinabhshek979@gmail.com>
2024-08-09 00:01:26 +0530

    nav ui

--- components/TopNavbar.jsx
+++ components/TopNavbar.jsx
@@ -17,29 +17,36 @@ const TopNavbar = () => {
   }, [pathname]);
 
   return (
-    <div className="w-screen max-[480px]:pl-0 max-[480px]:justify-center fixed bg-transparent flex justify-start gap-8 pl-[90px] h-[80px] items-center z-10">
+    <div
+      className={` max-[480px]:${
+        pathname === "/" ? "bg-[#10040b]" : "bg-transparent"
+      } w-screen max-[480px]:pl-0 max-[480px]:justify-center fixed bg-transparent flex justify-start gap-8 pl-[90px] h-[80px] items-center z-10`}
+    >
       <button
-        className=" w-max absolute left-1 flex justify-center items-center  mb-1"
+        className={`${color} w-max absolute left-1 flex justify-center items-center  mb-1`}
         onClick={() => router.push("/")}
       >
         <FaHome size={"1.3rem"} />
       </button>
 
-      <h4
+      <button
         onClick={() => router.push("/characters")}
         className={`${color} noto `}
       >
         CHARACTERS
-      </h4>
-      <h4
+      </button>
+      <button
         onClick={() => router.push("/randomiser")}
         className={`${color} noto `}
       >
         RANDOMISER
-      </h4>
-      <h4 onClick={() => router.push("/events")} className={`${color} noto `}>
+      </button>
+      <button
+        onClick={() => router.push("/events")}
+        className={`${color} noto `}
+      >
         NEWS & EVENTS
-      </h4>
+      </button>
     </div>
   );
 };
