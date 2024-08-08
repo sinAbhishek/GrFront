3337661e18edf7bd75755aaf667281c8943cffda
sinAbhishek <sinabhshek979@gmail.com>
2024-08-08 20:27:47 +0530

    added chars and home

--- components/TopNavbar.jsx
+++ components/TopNavbar.jsx
@@ -2,11 +2,18 @@
 
 import { useRouter } from "next/navigation";
 import React from "react";
-
+import { FaHome } from "react-icons/fa";
 const TopNavbar = () => {
   const router = useRouter();
   return (
     <div className="w-screen max-[480px]:pl-0 max-[480px]:justify-center fixed bg-transparent flex justify-start gap-8 pl-[90px] h-[80px] items-center z-10">
+      <button
+        className=" w-max absolute left-1 flex justify-center items-center  mb-1"
+        onClick={() => router.push("/")}
+      >
+        <FaHome size={"1.3rem"} />
+      </button>
+
       <h4
         onClick={() => router.push("/characters")}
         className=" noto text-slate-800"
