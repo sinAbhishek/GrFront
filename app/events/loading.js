ae97abf1b2079b4e639a7d9ce2dd81737813a8e9
sinAbhishek <singhabhshek979@gmail.com>
2024-08-09 17:39:43 +0530

    added loaders

---
+++ app/events/loading.js
@@ -0,0 +1,9 @@
+export default function Loading() {
+  // Or a custom loading skeleton component
+  return (
+    <div className=" w-screen h-screen flex justify-center items-center">
+      {" "}
+      <p className=" text-lg text-slate-900">Loading...</p>
+    </div>
+  );
+}
